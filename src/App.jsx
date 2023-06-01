e8bd045b18e82c663eae0d7753c74c7aa41badab
Mariano Acosta <marianoacosta200102@gmail.com>
2023-05-31 21:01:17 -0300

    Commit para examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import Card from './components/Card'
+import Card from './components/Card';
 
 function App() {
+  
+
   return (
     <div className="App">
       <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <Card/>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,40 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, {useState} from 'react'
+
 
 const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
-};
+    const [pelicula, setPelicula] = useState("");
+    const [actor, setActor] = useState("");
+    const [error, setError] = useState(false);
+    const [valido, setValido] = useState(false);
+    
+    const onChangePelicula = (e) => setPelicula(e.target.value)
+    
+    const onChangeActor = (e) => setActor(e.target.value)
+    
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (pelicula.trim().length < 3 || pelicula.trim().startsWith(' ') || actor.trim().length < 6) {
+            setError(true);
+            setValido(false);
+        } else {
+            setError(false);
+            setValido(true);
+        }
+    }
+    
+    return (
+    <div>
+        <h2>Indique la Pelicula y el interprete</h2>
+        <form onSubmit={onSubmitForm}>
+            <input type="text" placeholder='Nombre de la pelicula' value={pelicula} onChange={onChangePelicula} />
+            <input type="text" placeholder='Nombre del Actor' value={actor} onChange={onChangeActor}/>
+            <button type='submit'>Enviar</button>
+        </form>
+        {error && <p>Algun dato no es válido</p>}
+        {valido && !error && <Card pelicula={pelicula} actor={actor} />}
+    </div>
+  )
+}
 
-export default Card;
+export default Card
